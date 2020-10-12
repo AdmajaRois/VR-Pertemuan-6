4a63daced73bb89cf74371d2b34382447df3caed
AdmajaRois <rois.da.silva@gmail.com>
2020-10-12 20:20:25 +0700

    initializing

---
+++ Assets/Scripts/HalamanUtama.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HalamanUtama : MonoBehaviour
+{
+    public Text textHalaman;
+   public void pindahHalaman(string halaman)
+   {
+       if (halaman == "SceneTiga")
+       {
+           textHalaman.text = "Rois memasuki Game minggu ke-3";
+       }else if (halaman == "SceneEmpat")
+       {
+            textHalaman.text = "Rois memasuki Game minggu ke-4";
+       }else
+       {
+           textHalaman.text = "Rois memasuki Game minggu ke-5";
+       }
+       SceneManager.LoadScene(halaman);
+   }
+
+   public void keluarAplikasi()
+   {
+       Application.Quit();
+   }
+}
